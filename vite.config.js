6606d838c3530f1b481881c6071f60e046ce51ad
hannikagabrielsson <r.hannika@gmail.com>
2024-03-07 19:43:54 +0200

    correct vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/hannikagabrielsson-React-perusteet-osa2-maiden-tiedot",
+  base: "/React-perusteet-osa2-maiden-tiedot",
   plugins: [react()],
 })
